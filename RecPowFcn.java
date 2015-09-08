f23aacbb97f204dcc8f5d92c7dec3ead39fa5dcf
WilsonMathew <wilsonmathewone@gmail.com>
2015-09-07 21:43:51 -0700

    added comments and finished programming stype.

--- RecPowFcn.java
+++ RecPowFcn.java
@@ -1,19 +1,28 @@
+/*****************************************************
+* The power of x^k for k >=1 
+* using resursive approach. Thinking resursively
+* exercise 3.1.
+* Wilson Mateo.  
+******************************************************/
 public class RecPowFcn {
+
 	public static void main(String[] args) {
 	System.out.println("power: " + POWER(3,3));
 	}
 	
 	public static int POWER(int x, int k) {
-
+		// hold the value of x^k 
+		int result = 0;
+		
 		if(k == 1) { 
-		//	x = x; don't understand why it works for sure tho 
+			result = x;
 		} 
 		else { 
-			x *= POWER(x,k-1);
+			// recursive call
+			result = x*POWER(x,k-1);
 		}
 
-		return x;
-
+		return result;
 	}
 
 }
